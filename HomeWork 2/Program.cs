c3ca1cf3eef9d84c67d26a174410bca84cf8c00a
Leonik11 <nick.leonidze@gmail.com>
2024-01-16 12:52:59 +0400

    HomeWork

---
+++ HomeWork 2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Niko Leonidze");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(Console.ReadLine());
